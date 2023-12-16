d80dce05e97c3c257d400bd696dd367d6890c289
AhmadMdi <admimohammadi@gmail.com>
2023-12-16 14:23:37 +0330

    router put and delete

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/post',[PostController::class,'index']);
 Route::post('/post',[PostController::class,'store']);
 Route::get('/post/{post}',[PostController::class,'show']);
+Route::put('/post/{post}',[PostController::class,'update']);
+Route::delete('/post/{post}',[PostController::class,'destroy']);
