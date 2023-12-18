7b0b9f6d8bb2801de7a7a886cf8d1f37b9e6b142
AhmadMdi <admimohammadi@gmail.com>
2023-12-18 23:07:38 +0330

    change put to post for update image

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 use App\Http\Controllers\Post\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/post',[PostController::class,'index']);
 Route::post('/post',[PostController::class,'store']);
 Route::get('/post/{post}',[PostController::class,'show']);
-Route::put('/post/{post}',[PostController::class,'update']);
+Route::match(['post'],'/post/{post}',[PostController::class,'update']);
 Route::delete('/post/{post}',[PostController::class,'destroy']);
+//Route::resource('post',PostController::class);
