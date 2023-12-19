eb6a9865f7391f28474685af604fd74aafba090f
AhmadMdi <admimohammadi@gmail.com>
2023-12-19 23:49:11 +0330

    repository and service for auth

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,7 +2,10 @@
 
 namespace App\Repositories;
 
+use Illuminate\Http\Request;
+
 interface UserRepository
 {
     public function getAllUsers();
+    public function signup(array $userSignup);
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,11 +3,25 @@
 namespace App\Services;
 use App\Models\User;
 use App\Repositories\UserRepository;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
 
 class UserService implements UserRepository
 {
+    use ApiResponse;
     public function getAllUsers()
     {
         return   User::paginate(10)/*->load('posts')*/;
     }
+
+    public function signup(array $userSignup): \Illuminate\Http\JsonResponse
+    {
+        $user = User::query()->create($userSignup);
+        $token = $user->createToken('ahmad')->accessToken;
+        return $this->successResponse(200,[
+            'user' => $user,
+            'token' => $token,
+        ],'user ok');
+    }
+
 }
